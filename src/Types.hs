144362f9e6562316b976f0a10aaaec5b8e52e32a
David Young <dyoung@ittc.ku.edu>
2015-08-21 16:55:19 -0500

    Ideas for lambda translation

--- src/Types.hs
+++ src/Types.hs
@@ -1,38 +1,66 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
 
 module Types where
 
 import           Control.Monad
+import           Data.Proxy
+
+-- | de Bruijn indices
+type Name = Int
+
+data LamTy
 
 data E a where
-  LitB :: Bool -> E Bool
-  LitI :: Int  -> E Int
-  Not  :: E Bool -> E Bool
+  LitB   :: Bool                   -> E Bool
+  LitI   :: Int                    -> E Int
+  Not    :: E Bool                 -> E Bool
+  Var    :: Name                   -> E a
+  Lambda :: Name -> proxy t -> E r -> E LamTy -- XXX: Is this the type we want?
+  R      :: R a                    -> E (R a)
+
+getBinder :: E LamTy -> Name
+getBinder (Lambda n _ _) = n
+
+-- getBody :: E LamTy -> E r
+-- getBody (Lambda _ _ b) = b
+
+getArgProxy :: E LamTy -> Proxy t
+getArgProxy _ = Proxy
+
+subst :: E t -> E LamTy -> E r
+subst arg (Lambda n _ body) = go body
+  where
+    go (Var n') = arg
+
+-- TODO: Figure out capture avoidance.
+abstract :: (E t -> E r) -> E LamTy
+abstract f = Lambda 0 Proxy (f (Var 0))
 
 data Action a where
-  Button :: Int -> Action (E Bool)
+  Button :: Int           -> Action (E Bool)
   Led    :: Int -> E Bool -> Action ()
-  Wait   :: Int -> Action ()
-  LitA   :: E a -> Action a
+  Wait   :: Int           -> Action ()
+  LitA   :: E a           -> Action a
 
 data R a where
-  Action :: Action a -> R a
-  Bind   :: R a -> (a -> R b) -> R b
-  Return :: a -> R a
-  Loop   :: R () -> R ()
+  Action :: Action a             -> R a
+  Bind   :: R a -> (E (a -> R a)) -> R b
+  Return :: a                    -> R a
+  Loop   :: R ()                 -> R ()
   If     :: E Bool -> R a -> R a -> R a
 
--- {-# NOINLINE Action #-}   -- XXX: How do we do this?
+-- {-# NOINLINE Action #-}   -- XXX: How do we get rid of this warning?
 
-instance Functor R where
-  fmap = liftM
+-- instance Functor R where
+--   fmap = liftM
 
-instance Applicative R where
-  pure  = return
-  (<*>) = ap
+-- instance Applicative R where
+--   pure  = return
+--   (<*>) = ap
 
-instance Monad R where
-  return = Return
-  (>>=)  = Bind
-  a >> b = a >>= const b  -- XXX: Why is this needed to avoid a core lint warning?
+-- instance Monad R where
+--   return = Return
+--   (>>=)  = Bind
+--   a >> b = a >>= const b  -- XXX: Why is this needed to avoid a core lint warning?
 
