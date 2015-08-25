3058cd3b5759b698c96f93b67195ca711023806a
David Young <dyoung@ittc.ku.edu>
2015-08-25 16:27:51 -0500

    Tracing for 'If' evaluation is now printed properly

--- src/Send.hs
+++ src/Send.hs
@@ -66,12 +66,12 @@ sendR (Bind m f) = updateButtons >> sendR m >>= sendR . f
 sendR (Action a) = updateButtons >> runCommand a
 sendR (Loop m  ) = forever (updateButtons >> sendR m)
 sendR (If b t f)
-  | evalE b      = do
-    traceM "Evaluating if on server..."
-    sendR t
-  | otherwise    = do
-    traceM "Evaluating if on server..."
-    sendR f
+  | evalE b      =
+    trace "Evaluating if on server (true branch)..."
+      $ sendR t
+  | otherwise    =
+    trace "Evaluating if on server (false branch)..."
+      $ sendR f
 
 initUI :: Canvas ()
 initUI = do
